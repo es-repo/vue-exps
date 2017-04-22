1638b633799a9a0cf4d91e8611e6b191f5acf5d5
es-repo <eugene.github@gmail.com>
2017-04-22 23:27:19 +0300

    Fixed logic

--- game2048/game2048.js
+++ game2048/game2048.js
@@ -9,22 +9,23 @@
 				.map(function () { return 0 })
 		})
 
-	function cellIsEmpty(x, y) { return board[y][x] == 0 }
-	function cellsEqual(x1, y1, x2, y2) { return board[y1][x1] == board[y2][x2] }
-	function canMoveChip(xf, yf, xt, yt) { return cellIsEmpty(xt, yt) || cellsEqual(xf, yf, xt, yt) }
+	function cellIsEmpty(c) { return board[c.y][c.x] == 0 }
+	function cellsEqual(c1, c2) { return board[c1.y][c1.x] == board[c2.y][c2.x] }
+	function canMoveChip(cf, ct) { return cellIsEmpty(ct) || cellsEqual(cf, ct) }
 
-	function moveChip(xf, yf, xt, yt) {
-		board[yt][xt] += board[yf][xf]
-		board[yf][xf] = 0
+	function moveChip(cf, ct) {
+		board[ct.y][ct.x] += board[cf.y][cf.x]
+		board[cf.y][cf.x] = 0
 	}
 
 	function findRandomEmptyPos() {
 		var r = Math.floor(Math.random() * size2)
+		var c = {}
 		for (var i = size2; i > 0; i--) {
-			var y = Math.floor(r / size)
-			var x = r % size
-			if (cellIsEmpty(x, y))
-				return { x: x, y: y }
+			c.y = Math.floor(r / size)
+			c.x = r % size
+			if (cellIsEmpty(c))
+				return c
 			r++
 			if (r == size2)
 				r = 0
@@ -32,25 +33,49 @@
 		return null
 	}
 
-	function move(dx, dy) {
+	function rot0(c, x, y) {
+		c.x = x
+		c.y = y
+	}
+	function rot90(c, x, y) {
+		c.x = y
+		c.y = x
+	}
+	function rot180(c, x, y) {
+		c.x = size - 1 - x
+		c.y = y
+	}
+	function rot270(c, x, y) {
+		c.x = y
+		c.y = size - 1 - x
+	}
+
+	function move(rot) {
 		var shifts = []
-		var xs = dx == 1 ? size - 1 : 0
-		var ys = dy == 1 ? size - 1 : 0
-		var xj = dx == 0 ? 1 : -dx
-		var yi = dy == 0 ? 1 : -dy
-		for (var i = 0, y = ys; i < size; i++, y += yi) {
-			for (var j = 0, x = xs; j < size; j++, x += xj) {
-				if (!cellIsEmpty(x, y)) {
-					var tx = x, ty = y
-					while (tx + dx < size && tx + dx >= 0
-						&& ty + dy < size && ty + dy >= 0
-						&& canMoveChip(x, y, tx + dx, ty + dy)) {
-						tx += dx
-						ty += dy
+		var c = {}
+		var tc = {}
+		for (var y = 0; y < size; y++) {
+			var s = size
+			for (var x = size - 2; x >= 0; x--) {
+				rot(c, x, y)
+				if (!cellIsEmpty(c)) {
+					var tx = x
+					while (tx + 1 < s) {
+						rot(tc, tx + 1, y)
+						if (!cellIsEmpty(tc)) {
+							if (cellsEqual(c, tc)) {
+								tx++
+								s = tx
+							}
+							break
+						}
+						tx++
 					}
-					if (x != tx || y != ty) {
-						moveChip(x, y, tx, ty)
-						shifts.push({ fx: x, fy: y, tx: tx, ty: ty })
+
+					if (x != tx) {
+						rot(tc, tx, y)
+						moveChip(c, tc)
+						shifts.push({ from: c, to: tc })
 					}
 				}
 			}
@@ -72,22 +97,22 @@
 			}
 			return r
 		},
-		left: function () {
-			return move(-1, 0)
-		},
 		right: function () {
-			return move(1, 0)
-		},
-		up: function () {
-			return move(0, -1)
+			return move(rot0)
 		},
 		down: function () {
-			return move(0, 1)
+			return move(rot90)
+		},
+		left: function () {
+			return move(rot180)
+		},
+		up: function () {
+			return move(rot270)
 		},
 		isFull: function () {
-			for (var x = 0; x < size; x++)
-				for (var y = 0; y < size; y++)
-					if (cellIsEmpty(x, y))
+			for (var c = { x: 0 }; c.x < size; c.x++)
+				for (c.y = 0; c.y < size; c.y++)
+					if (cellIsEmpty(c))
 						return false
 			return true
 		}
