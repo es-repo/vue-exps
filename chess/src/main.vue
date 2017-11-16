9fb3837ab91bcbb5081000862ca30ecbbe64bbf2
Eugene Sirota <ES-Macbook@9.0.168.192.in-addr.arpa>
2017-11-16 21:15:56 +0200

    Prompt for user. Save user in local storage

--- chess/src/main.vue
+++ chess/src/main.vue
@@ -1,13 +1,20 @@
 <template>
   <main>
-    <game :db="db" :user="user"></game>
+    <prompt message='Your name?'
+            v-if="user==null"
+            @value-entered="createUser" />
+    <game :db="db"
+          :user="user"
+          v-if="user!=null"></game>
   </main>
 </template>
 
 <script module>
+import uuid from 'uuid'
 import Firebase from 'firebase'
 import 'firebase/firestore'
 import Game from './game.vue'
+import Prompt from './ui-utils/prompt.vue'
 
 Firebase.initializeApp({
   apiKey: 'AIzaSyC-63ApL_7XETn31Xp2cl3xDwATy-l2HYw',
@@ -16,14 +23,44 @@ Firebase.initializeApp({
 })
 
 export default {
-  components: { Game },
+  components: { Game, Prompt },
   data() {
-    const usrId = Math.random() * 10000 | 0
     return {
       db: Firebase.firestore(),
-      user: {
-        id: usrId,
-        name: 'vasya-' + usrId }
+      user: null
+    }
+  },
+
+  created(){
+    this.user = this.getUserFromLocalStorage()
+  },
+
+  methods: {
+    createUser(userName) {
+      this.user = { id: uuid(), name: userName }
+      this.saveUserToLocalStorage(this.user)
+    },
+
+    getUserFromLocalStorage(){
+      try{
+        const usrStr = localStorage.getItem('user')
+        if (usrStr != null){
+          return JSON.parse(usrStr)
+        }
+      }
+      catch(e){
+        return null
+      }
+    },
+
+    saveUserToLocalStorage(usr){
+      try{
+        const usrStr = JSON.stringify(usr)
+        localStorage.setItem('user', usrStr)
+      }
+      catch(e){
+        // Ignore
+      }
     }
   }
 }
---
+++ chess/src/ui-utils/prompt.vue
@@ -0,0 +1,106 @@
+<template>
+  <section :class="$style.container">
+    <div>{{message}}</div>
+    <div style="width:100%; display:flex;">
+      <input type="text"
+             ref="input"
+             maxlength="32"
+             v-model="value"
+             :class="$style.input"
+             @keyup.enter="onEnterPressed" />
+      <button :class="$style.button"
+              v-if="isValueValid"
+              ref="button"
+              @click="dismiss">
+        OK
+      </button>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    message: {
+      type: String,
+      default: ''
+    }
+  },
+
+  data() {
+    return {
+      value: ''
+    }
+  },
+
+  computed: {
+    isValueValid() {
+      return this.value.trim() != ''
+    }
+  },
+
+  mounted() {
+    this.$refs.input.focus()
+  },
+
+  methods: {
+    onEnterPressed(){
+      if (this.isValueValid)
+        this.$refs.button.click()
+    },
+
+    dismiss(){
+      this.$emit('value-entered', this.value)
+    }
+  }
+}
+</script>
+
+<style module>
+:root {
+  --first-color: white;
+  --second-color: hsl(191, 70%, 60%);
+  --text-color: #444;
+}
+
+.container {
+  width: 22em;
+  min-height: 5em;
+  height: auto;
+  border-radius: 0.3em;
+  outline: none;
+  border: none;
+  background-color: var(--first-color);
+  display: flex;
+  flex-direction: columns;
+  flex-wrap: wrap;
+  align-items: center;
+  padding: 0.5em;
+  color: var(--text-color);
+  font-size: 1.2rem;
+}
+
+.input {
+  width: 100%;
+  border: none;
+  border-bottom: 2px solid var(--second-color);
+  outline: none;
+  background: transparent;
+  font-size: 1.5rem;
+  color: var(--text-color);
+}
+
+.button {
+  border: none;
+  background-color: var(--second-color);
+  color: var(--first-color);
+  width: 4em;
+  outline: none;
+}
+
+.button:active {
+  color: var(--second-color);
+  background-color: var(--first-color);
+  border-bottom: 2px solid var(--second-color);
+}
+</style>
